d5c5e184d944d98c2271685e6e87c690edc68335
Your GitHub Username <itp26kkmm@gmail.com>
2026-04-22 11:34:20 +0200

    fix cors

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="XYZ to Molecule API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cheminformatics-nextjs.onrender.com"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
